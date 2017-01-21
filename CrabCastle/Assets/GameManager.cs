996d122132149b5a61a942cc34862bb0d1fdcd3c
gamejam <gamejam@coleman.local>
2017-01-21 12:14:54 -0800

    Pushing game manager script

---
+++ CrabCastle/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+	public bool towerPlacementActive;
+	public GameObject transTowerPrefab;
+
+	public int playerGold = 10000;
+
+	public Text playerGoldText; 
+
+	TowerPlacement tPlacement;
+
+	int towerCost = 100;
+
+	void Start () 
+	{
+		towerPlacementActive = false;
+		tPlacement = gameObject.GetComponent<TowerPlacement> () as TowerPlacement;
+	}
+
+	void Update()
+	{
+		playerGoldText.text = playerGold.ToString();
+	}
+
+	public void PlaceTower()
+	{
+		if (!towerPlacementActive && playerGold > towerCost) 
+		{
+			playerGold -= towerCost;
+			towerPlacementActive = true;
+			GameObject tTower = Instantiate (transTowerPrefab);
+			tPlacement.towerTransObj = tTower;
+		}
+	}
+}
